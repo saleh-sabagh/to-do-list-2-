2125e990c4c5e50cd6f92c7f668421f52e6d5bb4
saleh sabagh <saleh4532eh@gmail.com>
2025-10-14 00:07:01 +0330

    feat : add project creating option in CLI

--- src/to_do_list/main.py
+++ src/to_do_list/main.py
@@ -7,11 +7,32 @@ def show_menu():
     print("\n===== TO DO LIST MANAGER =====")
     print("1. Create new project")
     print("2. Add task to project")
-    print("3. Delete task from project")
-    print("4. List all projects")
-    print("5. List tasks in project")
-    print("6. Delete project")
+    print("3. Add task to project")    
+    print("4. Delete task from project")
+    print("5. List all projects")
+    print("6. List tasks in project")
+    print("7. Delete project")
     print("0. Exit")
     print("==============================")
     
+    
+def get_project_by_name(name : str) -> Project:
+    if name not in Project._projects_name:
+        raise ValueError("Project not found.")
+    return Project._projects_name[name]
+
+def main():
+    while True:
+        show_menu()
+        choice = input("Choose an option: ").strip()
+
+        try:
+            if choice == "1":
+                name = input("Project name: ").strip()
+                desc = input("Description: ").strip()
+                Project(name, desc)
+                print(f"✅ Project '{name}' created successfully.")
+
+        except Exception as e:
+            print(f"⚠️ Error: {e}")
 

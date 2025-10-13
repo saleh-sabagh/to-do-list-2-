3d3698403b138e7543c224dbb6a9e6ad4a5c7499
saleh sabagh <saleh4532eh@gmail.com>
2025-10-13 14:58:21 +0330

    feat(project): add validation for project name and description length

--- src/to_do_list/project.py
+++ src/to_do_list/project.py
@@ -7,7 +7,16 @@ class Project:
     MAX_PROJECTS = int(os.getenv("MAX_NUMBER_OF_PROJECT", 10))
     _projects_name = {}
     def __init__(self, name : str, description : str) -> None:
+        if name in Project._projects_name:
+            raise ValueError(f"Projec with name {name} already exists!")
+        if len(Project._projects_name) >= Project.MAX_PROJECTS:
+            raise ValueError(f"Cannot create more than {Project.MAX_PROJECTS} projects!")
+        if len(name) > 30 or len(name) < 1:            
+            raise ValueError("project's name must be less than 30 characters and not empty")
+        if len(description) > 150 or len(description) < 1:            
+            raise ValueError("project's description must be less than 150 characters and not empty")
         self.name = name
+        Project._projects_name[name] = self
         self.description = description
 
     def change_name(self, new_name : str):

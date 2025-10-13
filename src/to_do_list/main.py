94e29a68c677e5e38a9607e808df0078543bc4a1
saleh sabagh <saleh4532eh@gmail.com>
2025-10-14 01:58:04 +0330

    feat : add functions for choosing projects and tasks

--- src/to_do_list/main.py
+++ src/to_do_list/main.py
@@ -7,7 +7,7 @@ def show_menu():
     print("\n===== TO DO LIST MANAGER =====")
     print("1. Create new project")
     print("2. Add task to project")
-    print("3. Add task to project")    
+    print("3. Edit task")    
     print("4. Delete task from project")
     print("5. List all projects")
     print("6. List tasks in project")
@@ -20,6 +20,69 @@ def get_project_by_name(name : str) -> Project:
     if name not in Project._projects_name:
         raise ValueError("Project not found.")
     return Project._projects_name[name]
+
+
+def choose_project():
+    print("List of projects:")
+    projects = [(p.id, p.name) for p in Project._projects_name.values()]
+    for pid, name in projects:
+        print(f"{pid}. {name}")
+    print("_____________________")
+
+    try:
+        p_id = int(input("Select project's ID: ").strip())
+        project_name = next(name for pid, name in projects if p_id == pid)
+        return get_project_by_name(project_name)
+    except (ValueError, StopIteration):
+        print("Invalid project ID!")
+        return None
+
+
+def choose_task(project):
+    tasks = [(t.id, t.title, t) for t in project.all_project_tasks().values()]
+    if not tasks:
+        print("There are no tasks in this project.")
+        return None
+    print("List of tasks:")
+    for tid, title, _ in tasks:
+        print(f"{tid}. {title}")
+    print("_____________________")
+
+    try:
+        t_id = input("Enter task ID: ").strip()
+        
+        return next(t for tid, _, t in tasks if tid == t_id)
+    except (ValueError, StopIteration):
+        print("Invalid task ID!")
+        return None
+
+
+def edit_task(task):
+    print("What do you want to change?\n"
+          "1. Title\n"
+          "2. Description\n"
+          "3. Deadline")
+    try:
+        choice = int(input("Choose option: ").strip())
+    except ValueError:
+        print("Invalid choice.")
+        return
+
+    if choice == 1:
+        new_title = input("Enter new title: ").strip()
+        task.change_title(new_title)
+        print("✅ Title updated.")
+    elif choice == 2:
+        new_desc = input("Enter new description: ").strip()
+        task.change_description(new_desc)
+        print("✅ Description updated.")
+    elif choice == 3:
+        new_deadline = input("Enter new deadline (YYYY-MM-DD): ").strip()
+        task.change_deadline(new_deadline)
+        print("✅ Deadline updated.")
+    else:
+        print("Invalid option.")
+
 
 def main():
     while True:

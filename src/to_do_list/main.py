98c1055890695c7aee9203d9b0f82eddec2b48cd
saleh sabagh <saleh4532eh@gmail.com>
2025-10-13 23:56:31 +0330

    feat : add show-menu  ability

--- src/to_do_list/main.py
+++ src/to_do_list/main.py
@@ -3,3 +3,15 @@ from task import Task
 from dotenv import load_dotenv
 load_dotenv()
 
+def show_menu():
+    print("\n===== TO DO LIST MANAGER =====")
+    print("1. Create new project")
+    print("2. Add task to project")
+    print("3. Delete task from project")
+    print("4. List all projects")
+    print("5. List tasks in project")
+    print("6. Delete project")
+    print("0. Exit")
+    print("==============================")
+    
+

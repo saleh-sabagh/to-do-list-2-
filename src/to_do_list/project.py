394f10ecfac76e1663155f1694b39b61268ee3de
saleh sabagh <saleh4532eh@gmail.com>
2025-10-13 14:57:52 +0330

    feat(project): add MAX_PROJECTS limit and track existing project names

--- src/to_do_list/project.py
+++ src/to_do_list/project.py
@@ -1,6 +1,11 @@
+import os
+from dotenv import load_dotenv
 load_dotenv()
+
 class Project:
     
+    MAX_PROJECTS = int(os.getenv("MAX_NUMBER_OF_PROJECT", 10))
+    _projects_name = {}
     def __init__(self, name : str, description : str) -> None:
         self.name = name
         self.description = description

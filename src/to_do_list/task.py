286b28160924f3bffa60df6fcfda9c2b17fc9c4d
saleh sabagh <saleh4532eh@gmail.com>
2025-10-13 13:59:21 +0330

    feat: add description field to Task class

--- src/to_do_list/task.py
+++ src/to_do_list/task.py
@@ -2,12 +2,15 @@ from datetime import datetime
 
 class Task:
     
-    def __init__(self, title : str, deadline : datetime) -> None:
+    def __init__(self, title : str, description : str, deadline : datetime) -> None:
         if len(title) > 30 or len(title) < 1:            
-            raise ValueError("task title must be less than 30 characters and not empty")
+            raise ValueError("task's title must be less than 30 characters and not empty")
+        
+        if len(description) > 150 or len(description) < 1:            
+            raise ValueError("task's description must be less than 150 characters and not empty")
+        
         try:
             deadline_date = datetime.strptime(deadline, "%Y-%m-%d")
-            
         except ValueError:
             raise ValueError("Invalid date format. Use YYYY-MM-DD.")
         
@@ -15,5 +18,7 @@ class Task:
             raise ValueError("Deadline cannot be in the past.")
         
         self.title = title
+        self.description = description
         self.deadline = deadline
         self.status = "todo"
+

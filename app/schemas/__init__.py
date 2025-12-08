b0c227d36680b0444ec16469b8f380fd34bd04eb
saleh sabagh <saleh4532eh@gmail.com>
2025-12-08 13:33:36 +0330

    feat(api): add pydantic schemas

---
+++ app/schemas/__init__.py
@@ -0,0 +1,21 @@
+from app.schemas.project import (
+    ProjectCreate,
+    ProjectPartialUpdate,
+    ProjectRead,
+    ProjectUpdate,
+    ProjectWithTasks,
+)
+from app.schemas.task import TaskCreate, TaskPartialUpdate, TaskRead, TaskUpdate
+
+__all__ = [
+    "ProjectCreate",
+    "ProjectPartialUpdate",
+    "ProjectRead",
+    "ProjectUpdate",
+    "ProjectWithTasks",
+    "TaskCreate",
+    "TaskPartialUpdate",
+    "TaskRead",
+    "TaskUpdate",
+]
+
